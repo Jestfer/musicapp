4a66340e22a5dae2aa3c9a4118400447c875f898
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-11 23:28:15 +0000

    created scaffold and generating new tracks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  ### This will create routes for us to interact with
+  resources :tracks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
