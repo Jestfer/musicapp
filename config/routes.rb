314ceb7917a68c8462d3582a8a7a91c49c83dd0e
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-12 23:51:21 +0000

    added new playlists model. Next: work on the view, create forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   ### This will create routes for us to interact with
   resources :tracks
+  resources :playlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311225631) do
+ActiveRecord::Schema.define(version: 20180312232318) do
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.integer "number_of_votes"
+  end
 
   create_table "tracks", force: :cascade do |t|
     t.string "title"
