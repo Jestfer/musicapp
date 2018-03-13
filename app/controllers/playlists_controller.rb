60c126c0dd131443aec7848ac4d10e0a1001bde2
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-13 22:57:06 +0000

    added more routes and model validations!

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,6 +4,27 @@ class PlaylistsController < ApplicationController
   end
 
   def show
-    @playlists = Playlist.find(params[:id])
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    if @playlist.save
+      redirect_to @playlist
+    else
+      # Run validations, not empty! -> Added in model
+      render :new
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :number_of_votes)
   end
 end
