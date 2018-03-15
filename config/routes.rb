861fb232625cff5f9f3ce77a9d9fc50184e230bc
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-15 15:50:44 +0000

    added devise gem, authentication generation and also views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'spotify/search'
 
   ### This will create routes for us to interact with
   resources :tracks
   resources :playlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tracks#index'
 end
